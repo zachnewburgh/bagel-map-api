166011dd61b72e44ea78a3c701bc026087cffcaf
zachnewburgh <zach.newburgh@gmail.com>
2017-01-17 17:28:45 -0500

    Added infowindows to the location markers

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
 var initialLocations = [
   {
     lat: 40.7128,
-    lng: -74.0059
+    lng: -74.0059,
+    title: "New York",
+    content: "This is New York City.",
+    attribution: "http://www.nyc.gov/"
   },
   {
     lat: 40.7312,
-    lng: -73.9971
+    lng: -73.9971,
+    title: "Washington Square Park",
+    content: "This is WSQ Park.",
+    attribution: "http://www.wsq.gov/"
+
   }
 ]
 
 var Location = function(data) {
   this.lat = ko.observable(data.lat);
   this.lng = ko.observable(data.lng);
+  this.title = ko.observable(data.title);
+  this.content = ko.observable(data.content);
+  this.attribution = ko.observable(data.attribution);
 }
 
 var ViewModel = function() {
@@ -43,7 +53,27 @@ var ViewModel = function() {
         map = new google.maps.Map(element, mapOptions);
 
         // Instantiate a marker for each item in locationsList.
-        that.locationsList().forEach(function(locationItem) {
+        console.log(that.locationsList().length)
+
+        for (i = 0; i < that.locationsList().length; i++) { 
+          locationItem = that.locationsList()[i]
+
+          contentString = '<div class="content">'+
+            '<div class="siteNotice">'+
+            '</div>'+
+            '<h1 id="' + locationItem.title() + '" class="firstHeading">' + locationItem.title() + '</h1>'+
+            '<div class="bodyContent">'+
+            '<p>' + locationItem.content() + '</p>'+
+            '<p>Attribution: ' + locationItem.title() + 
+            ', <a href="' + locationItem.attribution() + '">' +
+            locationItem.attribution() +
+            '</a> (last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
+
+          infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
           
           // Create an object with lat and lng for each locationItem.
           markerLocation = {
@@ -53,10 +83,29 @@ var ViewModel = function() {
           // Instantiate a new marker for each locationItem.
           marker = new google.maps.Marker({
             position: markerLocation,
-            map: map
+            map: map,
+            infowindow: infowindow
+
           });
 
-        });
+          // Close all other infowindows when another is clicked.
+          lastOpenedInfowindow = ''
+
+          closeLastOpenedInfowindow = function() {
+            if (lastOpenedInfowindow) {
+              lastOpenedInfowindow.close();
+            }
+          }
+
+          // Provide markers with infowindow upon click.
+          google.maps.event.addListener(marker, 'click', function() {
+            closeLastOpenedInfowindow();
+            this.infowindow.open(map, this);
+            lastOpenedInfowindow = this.infowindow;
+          });
+
+          
+        };
     }
   };
 
