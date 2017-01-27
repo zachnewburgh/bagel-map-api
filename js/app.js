2c5feae82f80893646dda096a7e1e7b2a9fb8f19
zachnewburgh <zach.newburgh@gmail.com>
2017-01-27 15:51:36 -0500

    Changed the getJSON function to ajax with error handling; replaced many 'this' with 'that' in Location and ViewModel

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ var locationsList = ko.observableArray([]);
 var Location = function(data) {
   that = this;
 
-  this.lat = ko.observable(data.lat);
-  this.lng = ko.observable(data.lng);
-  this.title = ko.observable(data.title);
-  this.content = ko.observable(data.content);
-  this.attribution = ko.observable(data.attribution);
+  that.lat = ko.observable(data.lat);
+  that.lng = ko.observable(data.lng);
+  that.title = ko.observable(data.title);
+  that.content = ko.observable(data.content);
+  that.attribution = ko.observable(data.attribution);
 
-  this.contentString = 
+  that.contentString = 
     '<div class="content">'+
     '<h1 id="' + that.title() + '" class="firstHeading">' + that.title() + '</h1>'+
     '<dataiv class="bodyContent">'+
@@ -20,18 +20,18 @@ var Location = function(data) {
     '</div>';
 
   // Create an infowindow for each location.
-  this.infowindow = new google.maps.InfoWindow({
+  that.infowindow = new google.maps.InfoWindow({
     content: that.contentString
   });
   
   // Create an object with lat and lng for each locationItem.
-  this.markerLocation = {
+  that.markerLocation = {
     lat: that.lat(),
     lng: that.lng()
   };
 
   // Instantiate a new marker for each locationItem.
-  this.marker = new google.maps.Marker({
+  that.marker = new google.maps.Marker({
     title: that.title(),
     position: that.markerLocation,
     map: map,
@@ -40,11 +40,11 @@ var Location = function(data) {
   });
 
   // Hide the marker from the map.
-  this.marker.setVisible(false);
+  that.marker.setVisible(false);
 
   // On click, show/hide the marker's infowindow
   // and start/stop the marker's bounce animation.
-  this.marker.addListener('click', function() {
+  that.marker.addListener('click', function() {
     selectedMarkerActions(this)
   });
 
@@ -79,6 +79,7 @@ var Location = function(data) {
 var ViewModel = function() {
   var that = this;
 
+  // Foursquare API base URL and parameters.
   fs_base = 'https://api.foursquare.com/v2/venues/search?near=New%20York,NY';
   fs_id = '&client_id=H5KKDQATDVABW3XSDBQ043GVRIQYCUII1SHKUAOVK2MUNA0P';
   fs_secret = '&client_secret=L5WYBLGJSGG4OCC4AJ5XAIWDIPM1DJRCVWZSRWAKOFJVQKUN';
@@ -87,30 +88,41 @@ var ViewModel = function() {
   fs_mode = '&m=swarm';
   fs_call = fs_base + fs_query + fs_version + fs_mode + fs_id + fs_secret;
 
-  $.getJSON(fs_call, function(data) {
-    data.response.venues.slice(0,30).forEach(function(venue) {
-      locationsList.push(new Location({
-        "title": venue.name,
-        "lat": venue.location.lat,
-        "lng": venue.location.lng,
-        "lng": venue.location.lng,
-        "content": venue.categories[0].name,
-        "attribution": venue.url||'https://www.google.com/#safe=off&q=' + venue.name + ' ' + venue.location.address
-      }));
-    });
+  // Using AJAX to scrape Foursquare's API to return a JSON object.
+  // If successful, for each venue, create a new Location. 
+  // Otherwise, display error message in the #error div.
+  $.ajax({
+    url: fs_call,
+    dataType: 'json',
+    success: function(data) {
+      data.response.venues.slice(0,30).forEach(function(venue) {
+        locationsList.push(new Location({
+          "title": venue.name,
+          "lat": venue.location.lat,
+          "lng": venue.location.lng,
+          "lng": venue.location.lng,
+          "content": venue.categories[0].name,
+          "attribution": venue.url||'https://www.google.com/#safe=off&q=' + venue.name + ' ' + venue.location.address
+        }));
+      });
+    },
+    error: function(data) {
+      errorDiv = document.getElementById('error');
+      errorDiv.innerHTML = ("<h2>Foursquare's API failed to load. Check your connection and refresh the page.</h2>");
+    }
   });
 
   // Links with the text input. 
-  this.searchQuery = ko.observable("");
+  that.searchQuery = ko.observable("");
 
   // Filter locations based on searchQuery and
   // set marker's visibility based on filter.
-  this.filteredLocations = ko.computed(function() {
+  that.filteredLocations = ko.computed(function() {
     var locationsArray = locationsList();
     return ko.utils.arrayFilter(locationsArray, function(locationItem) {
       if (locationItem.title().toLowerCase().includes(that.searchQuery().toLowerCase())) {
         locationItem.marker.setVisible(true);
-        return locationItem
+        return locationItem;
       } else {
         locationItem.marker.setVisible(false);
       };
@@ -119,7 +131,7 @@ var ViewModel = function() {
 
   // When clicking on a location in the list,
   // make the relevant marker bounce and infowindow open.
-  this.selectMarker = function(locationItem) {
+  that.selectMarker = function(locationItem) {
     let marker = locationItem.marker;
     selectedMarkerActions(marker);
   }
